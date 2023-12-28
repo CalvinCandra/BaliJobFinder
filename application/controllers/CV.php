33e32c5f2fa2e827f680da7d06eb022bd16b0e61
CalvinCandra <willycandra850@gmail.com>
2023-12-28 13:00:39 +0800

    Generate CV Otomatis

---
+++ application/controllers/CV.php
@@ -0,0 +1,98 @@
+<?php
+
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class CV extends CI_Controller {
+    public function __construct(){
+        parent:: __construct();
+
+        $this->load->model(array('M_auth', 'M_CV', 'M_pelamar'));
+
+        $this->load->library('form_validation'); 
+    }
+
+    public function GenerateCV(){
+
+        // get session email
+        $email = $this->session->userdata('email');
+
+        //get user
+        $pelamar = $this->M_auth->getUser($email);
+
+        //get data-data pelamar
+        $data_pelamar = $this->M_pelamar->getDataPelamar($pelamar->id_users)->row();
+
+        // get data pendidikan
+        $data_pendidikan = $this->M_CV->getDataPendidikan($data_pelamar->id_pelamar)->result_array();
+        
+        // get data pengalaman
+        $data_pengalaman = $this->M_CV->getDataPengalaman($data_pelamar->id_pelamar)->result_array();
+        
+        // get data skill
+        $data_skill = $this->M_CV->getDataSkill($data_pelamar->id_pelamar)->result_array();
+
+        // DOMPDF
+        $this->load->library('pdfgenerator');
+
+        $data['title'] = "Curriculum vitae";
+
+        $file_pdf = $data['title'];
+
+        $paper = 'A4';
+
+        $orientation = "portrait";
+
+        $data=array(
+            'email_pelamar' => $pelamar->email,
+            'nama_pelamar' => $data_pelamar->nama_lengkap,
+            'deskripsi_pelamar' =>$data_pelamar->deskripsi_pelamar,
+            'no_hp' =>$data_pelamar->no_hp,
+            'alamat' =>$data_pelamar->alamat,
+            'pendidikan' =>$data_pendidikan,
+            'pengalaman' =>$data_pengalaman,
+            'skill' =>$data_skill,
+        );
+
+        $html =  $this->load->view('Cv/LayoutCVNew', $data, TRUE);
+
+        $this->pdfgenerator->generate($html, $file_pdf, $paper, $orientation);
+    }
+
+    public function index(){
+        // get session email
+        $email = $this->session->userdata('email');
+
+        //get user
+        $pelamar = $this->M_auth->getUser($email);
+
+        //get data-data pelamar
+        $data_pelamar = $this->M_pelamar->getDataPelamar($pelamar->id_users)->row();
+
+        // get data pendidikan
+        $data_pendidikan = $this->M_CV->getDataPendidikan($data_pelamar->id_pelamar)->result_array();
+        
+        // get data pengalaman
+        $data_pengalaman = $this->M_CV->getDataPengalaman($data_pelamar->id_pelamar)->result_array();
+        
+        // get data skill
+        $data_skill = $this->M_CV->getDataSkill($data_pelamar->id_pelamar)->result_array();
+
+        $data=array(
+            'email_pelamar' => $pelamar->email,
+            'nama_pelamar' => $data_pelamar->nama_lengkap,
+            'deskripsi_pelamar' =>$data_pelamar->deskripsi_pelamar,
+            'no_hp' =>$data_pelamar->no_hp,
+            'alamat' =>$data_pelamar->alamat,
+            'pendidikan' =>$data_pendidikan,
+            'pengalaman' =>$data_pengalaman,
+            'skill' =>$data_skill,
+        );
+
+        $this->load->view('Cv/LayoutCVNew', $data);
+
+    }
+
+}
+
+/* End of file CV.php */
