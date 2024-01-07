8975b230a159236784a3790606a2d8e9d9ca39f2
CalvinCandra <willycandra850@gmail.com>
2024-01-07 10:17:41 +0800

    fix bug

--- application/views/auth/forget/forget.php
+++ application/views/auth/forget/forget.php
@@ -2,26 +2,6 @@
     <div class="bg">
         <div class="containerr">
             <h2 class="judul fw-bold">Forget Password</h2> 
-
-                    $pesan = $this->session->flashdata('pesan');
-                    $error = $this->session->flashdata('error');
-
-                    if(isset($pesan)){
-                        echo '<div class="alert alert-success alert-dismissible fade show mt-3 mx-3 rounded-2" role="alert">
-                        <strong>'.$pesan.'</strong>.
-                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                    </div>';
-                    $this->session->unset_userdata('pesan');
-                    }
-
-                    if(isset($error)){
-                        echo '<div class="alert alert-danger alert-dismissible fade show mt-3 mx-3 rounded-2" role="alert">
-                        <strong>'.$error.'</strong>.
-                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                    </div>';
-                    $this->session->unset_userdata('error');
-                    }
-                ?>
             <div class="fomulir">
                 <form action="<?php echo base_url("Auth/KirimEmailPassForget")?>" method="post">
 
